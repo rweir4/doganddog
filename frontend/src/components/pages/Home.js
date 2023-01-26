e61e0c040e44bdabb3f5c9b628fa6c74d4ca4334
rweir4 <rebeccaweir12@gmail.com>
2023-01-26 16:22:56 -0500

    Add sections to home

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -3,10 +3,30 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className='intro'>
-      <img
-        src='/images/ginnytoy.png'
-        alt='logo' />
+    <div>
+      <div className='intro'>
+        <img
+          src='/images/ginnytoy.png'
+          alt='logo' />
+      </div>
+      <div className='content'>
+        <div className='ambassadors'>
+          <h1>Meet Our Ambassadors</h1>
+        </div>
+        <div className='featured-looks'>
+          <h1>Featured Looks</h1>
+            <div>
+              <img src='/images/GinnyInMock.png' alt='mock up' />
+              <h3>Classic Linen Tunic</h3>
+            </div>
+        </div>
+        <div className='email-list'>
+          <h1>Sign up for our Newsletter</h1>
+        </div>
+        <div className='socials'>
+          <h1>We're on Instagram!</h1>
+        </div>
+      </div>
     </div>
   );
 };

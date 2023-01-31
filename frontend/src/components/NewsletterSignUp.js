0fa05dab5fb3b5d24e2c995b59d37567169bed93
rweir4 <rebeccaweir12@gmail.com>
2023-01-30 22:38:12 -0500

    Add subscription form

--- frontend/src/components/NewsletterSignUp.js
+++ frontend/src/components/NewsletterSignUp.js
@@ -1,8 +1,23 @@
+// import e from "express";
 import React from "react";
+import { useState } from 'react';
 
 const NewsletterSignUp = () => {
+  let [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    // create store, create actions, and import this action
+    // saveNewSubscription(email);
+    e.preventDefault();
+  }
+
   return (
-    <div>Sign up for our newsletter</div>
+    <form onSubmit={e => handleSubmit(e)}>
+      <label>Sign up for our newsletter
+        <input type='text' value={email} onChange={e => setEmail(e.target.value)}/>
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
   );
 };
 

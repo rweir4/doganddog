275ac5d34da8b45b1eec2d3037a647801951935f
rweir4 <rebeccaweir12@gmail.com>
2023-02-01 16:39:14 -0500

    Make return smart, fix css

--- frontend/src/components/pages/ShippingAndReturns.js
+++ frontend/src/components/pages/ShippingAndReturns.js
@@ -1,43 +1,40 @@
 import React from "react";
 
+const ShippingPolicy = [
+  'We ship with USPS, and follow Etsy guidelines.',
+  'Domestic shipping is included in the price, and should take around one week.',
+  'International shipping will be an extra _____ amount, and will take two weeks.',
+]
+
+const ReturnPolicy = [
+  'Returns are accepted within two weeks of purchase for a full refund.',
+  'Package must be postmarked within 14 days of delivery.',
+  'All items must be brand new, unwashed, and hair-free, and must be free of wear and tear.',
+  'We do not accept exchanges, in order to ensure that items are in stock.',
+  'We do not provide return labels. The customer must pay for their own return label.',
+]
+
 const ShippingAndReturns = () => {
+  let shipping_count = 0
+  let return_count = 0
+  
   return (
     <div id='page-content' className='shipping-returns font-large'>
       <h1>Shipping Policy</h1>
-        <ul>
-          <li>
-            We ship with USPS, and follow Etsy guidelines.
-          </li>
-          <li>
-            Domestic shipping is included in the price, and should take around one week.
-          </li>
-          <li>
-            International shipping will be an extra _____ amount, and will take two weeks.
-          </li>
-        </ul>
+      {ShippingPolicy.map(policy => {
+        shipping_count += 1;
+        return (<li key={shipping_count}>{policy}</li>);
+      })}
       <div>
         <h1>Return Policy</h1>
-        <ul>
-          <li>
-            Returns are accepted within two weeks of purchase for a full refund.
-          </li>
-          <li>
-            Package must be postmarked within 14 days of delivery.
-          </li>
-          <li>
-            All items must be brand new, unwashed, and hair-free, and must be free of wear and tear.
-          </li>
-          <li>
-            We do not accept exchanges, in order to ensure that items are in stock.
-          </li>
-          <li>
-            We do not provide return labels. The customer must pay for their own return label.
-          </li>
-        </ul>
-        <div>
-          **Exceptions:
-          We cannot be responsible for lost or stolen packages for orders less than $25.
-        </div>
+        {ReturnPolicy.map(policy => {
+          return_count += 1;
+          return (<li key={return_count}>{policy}</li>);
+        })}
+      </div>
+      <div className='space-before'>
+        **Exceptions:
+        We cannot be responsible for lost or stolen packages for orders less than $25.
       </div>
     </div>
   );

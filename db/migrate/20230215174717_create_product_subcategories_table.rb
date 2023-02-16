c30376fe23e300383e4e5569e9815b962d911da0
rweir4 <rebeccaweir12@gmail.com>
2023-02-16 15:52:44 -0500

    Fix migraiton name

--- db/migrate/20230215174717_create_product_subcategories_table.rb
+++ db/migrate/20230215174717_create_product_subcategories_table.rb
@@ -1,4 +1,4 @@
-class CreateProductSubcategoriestable < ActiveRecord::Migration[7.0]
+class CreateProductSubcategories < ActiveRecord::Migration[7.0]
   def change
     create_table :product_subcategories do |t|
       t.string :subcategory, null: false
